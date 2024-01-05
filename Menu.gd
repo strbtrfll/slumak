7e4b38ebc172b884b2bf1f6796840a464377617d
strbtrfll <strbtrfll@gmail.com>
2024-01-05 15:26:58 +0200

    DLC

--- Menu.gd
+++ Menu.gd
@@ -5,7 +5,7 @@ func _ready():
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn") #указать путь к сцене(перетащить файл сцены в скобки())
+	get_tree().change_scene_to_file("res://main.tscn")#указать путь к сцене(перетащить файл сцены в скобки())
 
 func _on_option_button_pressed():
 	pass # Replace with function body.
