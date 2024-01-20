13fd33849445dab9874bf4b228fef988b163c55c
strbtrfll <strbtrfll@gmail.com>
2024-01-20 14:53:16 +0200

    Смерть

--- Сцени слимаків/Zelenui.gd
+++ Сцени слимаків/Zelenui.gd
@@ -5,7 +5,7 @@ var timer = 0
 var velocity = Vector2(randf_range(-10, 10), randf_range(-10, 10)) * speed
 var n = 0
 
-
+#Кожен кадр/рух
 func _process(delta):
 	# Двигаем объект в текущем направлении
 	if timer > randf_range(60, 250):
@@ -14,15 +14,28 @@ func _process(delta):
 	linear_velocity = velocity
 	timer = timer+1
 
+#Клік
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and shape_idx == 0:
 		n=n+1
 		if event.is_pressed() and n>1:
 			n=0
 			Save.score += 1
-			queue_free()
+			death()
 
+#Відштовхування
 func _on_body_entered(body):
 	velocity = Vector2(randf_range(-10, 10), randf_range(-10, 10)) * 12
 	timer = 0
-	
+
+#Таймер смерті
+func _on_death_timer_timeout():
+	queue_free()
+
+#Анімація смерті
+func death():
+	$AnimatedSprite2D.play("Death")
+	$DeathTimer.start()
+	$AnimationPlayer.play("Orange_death")
+	$CollisionShape2D.disabled = true
+	freeze = true
